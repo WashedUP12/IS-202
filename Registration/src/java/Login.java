28d5690ac4335f95556f5932eb0f948e20e20859
christoffer <christoffer>
2017-10-22 16:45:42 +0200

    clean up

---
+++ Registration/src/java/Login.java
@@ -0,0 +1,118 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author eriky
+ */
+public class Login extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+           String name = request.getParameter("user");
+           String password = request.getParameter("password");
+           String dbName = null;
+           String dbPassword = null;
+           String sql ="select * from users where name=? and password=?";
+           Class.forName("com.mysql.jdbc.Driver");
+           Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/webshop","root","DittPassord");
+           PreparedStatement ps = con.prepareStatement(sql);
+           ps.setString(1, name);
+           ps.setString(2, password);
+           ResultSet rs = ps.executeQuery();
+           while(rs.next()){
+               dbName = rs.getString("name");
+               dbPassword = rs.getString("password");
+           }
+           if (name.equals (dbName)&&password.equals(dbPassword)){
+               out.println("Successfull Login");
+           }
+           else{
+               response.sendRedirect("Login.jsp");
+               RequestDispatcher rd = request.getRequestDispatcher("Login.jsp");
+               rd.include (request, response);
+           }
+           
+           
+           
+           
+           
+           
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Registration.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(Registration.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ Registration/web/Studentoversikt.jsp
@@ -0,0 +1,93 @@
+<%@page language="java" import="java.util.*" %>
+<html>
+<head>
+<style>
+div.container {
+    width: 80%;
+    
+}
+
+header, footer {
+    padding: 1em;
+    color: white;
+    background-color: brown;
+    clear: left;
+    text-align: center;
+}
+
+nav {
+    float: left;
+    max-width: 160px;
+    margin: 0;
+    padding: 1em;
+}
+
+nav ul {
+    list-style-type: none;
+    padding: 0;
+}
+   
+nav ul a {
+    text-decoration: none;
+}
+
+table {
+    margin-left: 170px;
+    border-left: 1px solid gray;
+    padding: 1em;
+    overflow: hidden;
+}
+</style>
+</head>
+<body>
+
+<div class="container">
+
+<header>
+   <h1>SLIT</h1>
+   <p1>Studenter</p1>
+</header>
+  
+<nav>
+  <ul>
+     <li><a href="studentlist">Studenter</a></li>
+    <li><a href="#">Moduler</a></li>
+    <li><a href="#">Blogg</a></li>
+    <li><a href="#">Meldinger</a></li><br>
+    <li><a href="#">Logg ut</a></li>
+  </ul>
+</nav>
+<table>
+  <tr>
+    <th>Bruker ID</th>
+    <th>Elev</th>
+    <th>Rolle</th>
+    <th>E-post</th>
+    <th>Modul</th>
+  </tr>
+<% Iterator itr;%>
+<% List data=(List)request.getAttribute("data");
+for(itr=data.iterator(); itr.hasNext(); ){
+%>
+  <tr>
+    <td><%=itr.next()%></td>
+    <td><%=itr.next()%> &ensp;</td>
+    <td>Student &ensp;</td>
+    <td><%=itr.next()%></td>
+    <td>
+		<a href="StudentSelection.html">
+			<div style="height:100%;width:100%">
+				&#8680;
+			</div>
+		</a>
+	</td>
+  </tr>
+<%}%>
+</table>
+
+<footer>Kanskje en copyright?</footer>
+
+</div>
+
+</body>
+</html>
